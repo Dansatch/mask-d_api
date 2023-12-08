fa5ba7a32283b53f607f40655232fba2e95a1915
Dansatch <dansatch98@gmail.com>
2023-12-08 19:56:33 +0000

    Build auth route

---
+++ src/routes/auth.ts
@@ -0,0 +1,32 @@
+import express, { Request, Response } from "express";
+import bcrypt from "bcrypt";
+import Joi from "joi";
+import User from "../models/user";
+
+const router = express.Router();
+
+router.post("/", async (req: Request, res: Response) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  let user = await User.findOne({ username: req.body.username });
+  if (!user) return res.status(400).send("Invalid username or password.");
+
+  const validPassword = await bcrypt.compare(req.body.password, user.password);
+  if (!validPassword)
+    return res.status(400).send("Invalid username or password.");
+
+  const token = user.generateAuthToken();
+  res.header("x-auth-token", token).send(token);
+});
+
+function validate(req: any) {
+  const schema = Joi.object({
+    username: Joi.string().required(),
+    password: Joi.string().required(),
+  });
+
+  return schema.validate(req);
+}
+
+export default router;
